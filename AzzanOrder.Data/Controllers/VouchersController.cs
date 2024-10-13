c4f8f3392a8eca088e7bdcaa916b267d2754f054
Nguyễn Quang Minh <nguyenminh7902@gmail.com>
2024-10-14 00:32:46 +0700

    A VochersController

---
+++ AzzanOrder.Data/Controllers/VouchersController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AzzanOrder.Data.Models;
+
+namespace AzzanOrder.Data.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VouchersController : ControllerBase
+    {
+        private readonly OrderingAssistSystemContext _context;
+
+        public VouchersController(OrderingAssistSystemContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Vouchers
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Voucher>>> GetVouchers()
+        {
+          if (_context.Vouchers == null)
+          {
+              return NotFound();
+          }
+            return await _context.Vouchers.ToListAsync();
+        }
+
+        // GET: api/Vouchers/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Voucher>> GetVoucher(int id)
+        {
+          if (_context.Vouchers == null)
+          {
+              return NotFound();
+          }
+            var voucher = await _context.Vouchers.FindAsync(id);
+
+            if (voucher == null)
+            {
+                return NotFound();
+            }
+
+            return voucher;
+        }
+
+        // PUT: api/Vouchers/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutVoucher(int id, Voucher voucher)
+        {
+            if (id != voucher.VocherDetailId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(voucher).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!VoucherExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Vouchers
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Voucher>> PostVoucher(Voucher voucher)
+        {
+          if (_context.Vouchers == null)
+          {
+              return Problem("Entity set 'OrderingAssistSystemContext.Vouchers'  is null.");
+          }
+            _context.Vouchers.Add(voucher);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (VoucherExists(voucher.VocherDetailId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetVoucher", new { id = voucher.VocherDetailId }, voucher);
+        }
+
+        // DELETE: api/Vouchers/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVoucher(int id)
+        {
+            if (_context.Vouchers == null)
+            {
+                return NotFound();
+            }
+            var voucher = await _context.Vouchers.FindAsync(id);
+            if (voucher == null)
+            {
+                return NotFound();
+            }
+
+            _context.Vouchers.Remove(voucher);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool VoucherExists(int id)
+        {
+            return (_context.Vouchers?.Any(e => e.VocherDetailId == id)).GetValueOrDefault();
+        }
+    }
+}
