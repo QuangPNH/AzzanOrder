e101a710aa95fe840b0ad76ffe003fa1e126060e
Nguyễn Quang Minh <125878139+nguyenminh79@users.noreply.github.com>
2024-10-13 23:17:36 +0700

    Modify Models, Controllers Final DB

---
+++ AzzanOrder.Data/Controllers/PromotionsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AzzanOrder.Data.Models;
+
+namespace AzzanOrder.Data.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PromotionsController : ControllerBase
+    {
+        private readonly OrderingAssistSystemContext _context;
+
+        public PromotionsController(OrderingAssistSystemContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Promotions
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Promotion>>> GetPromotions()
+        {
+          if (_context.Promotions == null)
+          {
+              return NotFound();
+          }
+            return await _context.Promotions.ToListAsync();
+        }
+
+        // GET: api/Promotions/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Promotion>> GetPromotion(int id)
+        {
+          if (_context.Promotions == null)
+          {
+              return NotFound();
+          }
+            var promotion = await _context.Promotions.FindAsync(id);
+
+            if (promotion == null)
+            {
+                return NotFound();
+            }
+
+            return promotion;
+        }
+
+        // PUT: api/Promotions/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPromotion(int id, Promotion promotion)
+        {
+            if (id != promotion.PromotionId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(promotion).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PromotionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Promotions
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Promotion>> PostPromotion(Promotion promotion)
+        {
+          if (_context.Promotions == null)
+          {
+              return Problem("Entity set 'OrderingAssistSystemContext.Promotions'  is null.");
+          }
+            _context.Promotions.Add(promotion);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPromotion", new { id = promotion.PromotionId }, promotion);
+        }
+
+        // DELETE: api/Promotions/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePromotion(int id)
+        {
+            if (_context.Promotions == null)
+            {
+                return NotFound();
+            }
+            var promotion = await _context.Promotions.FindAsync(id);
+            if (promotion == null)
+            {
+                return NotFound();
+            }
+
+            _context.Promotions.Remove(promotion);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PromotionExists(int id)
+        {
+            return (_context.Promotions?.Any(e => e.PromotionId == id)).GetValueOrDefault();
+        }
+    }
+}
