a4bbab69a5aaa1d74fa2bd197d9065f937c9fb47
Vipxpert <vipxpert3721@gmail.com>
2024-10-15 13:14:02 +0700

    mao

--- AzzanOrder.Data/Models/Feedback.cs
+++ AzzanOrder.Data/Models/Feedback.cs
@@ -8,7 +8,6 @@ namespace AzzanOrder.Data.Models
         public int Feedbackid { get; set; }
         public string? Content { get; set; }
         public int? MemberId { get; set; }
-
         public virtual Member? Member { get; set; }
     }
 }
