915233cbc960fa3e512f2cb2788b606bacffd600
Vipxpert <vipxpert3721@gmail.com>
2024-10-31 13:20:13 +0700

    Owner List Employee

---
+++ AzzanOrder.ManagerOwner/Models/Model.cs
@@ -0,0 +1,11 @@
+﻿using AzzanOrder.Data.Models;
+
+namespace AzzanOrder.ManagerOwner.Models
+{
+    public class Model
+    {
+        public IEnumerable<Employee> employees { get; set; }
+
+        public Employee employee { get; set; }
+    }
+}
