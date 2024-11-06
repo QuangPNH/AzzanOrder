70eca7ee33b506070fc5eddcce135646abbf5c32
Thanh <daothanh1121@gmail.com>
2024-11-06 12:01:57 +0700

    Layout Changes, Admin Pages UI Addition

---
+++ AzzanOrder.ManagerOwner/Controllers/ManagerController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace AzzanOrder.ManagerOwner.Controllers
+{
+    public class ManagerController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        // POST: Employee/Add
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult AddPost()
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("List");
+            }
+
+            // If model validation fails, redisplay the form with validation messages
+            return View();
+        }
+
+        public IActionResult Update()
+        {
+            return View();
+        }
+
+        // POST: Employee/Update
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult UpdatePost()
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Update");
+            }
+
+            // If model validation fails, redisplay the form with validation messages
+            return View();
+        }
+
+        public IActionResult MenuList()
+        {
+            return View();
+        }
+
+        public IActionResult MenuItemList()
+        {
+            return View();
+        }
+
+        public IActionResult MenuItemDetail()
+        {
+            return View();
+        }
+
+
+    }
+}
