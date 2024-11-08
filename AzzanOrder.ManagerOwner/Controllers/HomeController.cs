311b910cadd3a00443c5f91fcd41d65172b2e6eb
Vipxpert <vipxpert3721@gmail.com>
2024-11-08 20:30:15 +0700

    Subscription payment progress

--- AzzanOrder.ManagerOwner/Controllers/HomeController.cs
+++ AzzanOrder.ManagerOwner/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using AzzanOrder.ManagerOwner.Models;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System.Diagnostics;
 using System.Numerics;
 using System.Reflection;
@@ -27,89 +28,89 @@ namespace AzzanOrder.ManagerOwner.Controllers
             return View();
         }
 
-		//00867f56a5a886a975463d3ec7941061
-		public IActionResult Privacy()
+        //00867f56a5a886a975463d3ec7941061
+        public IActionResult Privacy()
         {
             return View();
         }
 
 
 
-		[HttpGet]
-		public async Task<IActionResult> Login()
-		{
-			HttpContext.Request.Cookies.TryGetValue("LoginInfo", out string empJson);
-			Console.WriteLine("sfdjhsbfjs " + empJson);
-			if (await CheckLogin())
-			{
-				return RedirectToAction("List", "Employee");
-			}
-			return View();
-		}
+        [HttpGet]
+        public async Task<IActionResult> Login()
+        {
+            HttpContext.Request.Cookies.TryGetValue("LoginInfo", out string empJson);
+            Console.WriteLine("sfdjhsbfjs " + empJson);
+            if (await CheckLogin())
+            {
+                return RedirectToAction("List", "Employee");
+            }
+            return View();
+        }
 
 
-		[HttpPost]
-		public async Task<IActionResult> LoginAction(Employee employee)
-		{
+        [HttpPost]
+        public async Task<IActionResult> LoginAction(Employee employee)
+        {
             bool isManager;
-			Employee emp = null;
+            Employee emp = null;
             Owner owner = null;
-			using (HttpClient client = new HttpClient())
-			{
-				try
-				{
-					HttpResponseMessage res = await client.GetAsync(_apiUrl + "Employee/Manager/Phone/" + employee.Phone);
-					if (res.IsSuccessStatusCode)
-					{
-						string data = await res.Content.ReadAsStringAsync();
-						emp = JsonConvert.DeserializeObject<Employee>(data);
-					}
-					else
-					{
-						ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
-					}
-				}
-				catch (HttpRequestException e)
-				{
-					ModelState.AddModelError(string.Empty, "Request error. Please contact administrator.");
-				}
-			}
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage res = await client.GetAsync(_apiUrl + "Employee/Manager/Phone/" + employee.Phone);
+                    if (res.IsSuccessStatusCode)
+                    {
+                        string data = await res.Content.ReadAsStringAsync();
+                        emp = JsonConvert.DeserializeObject<Employee>(data);
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                    }
+                }
+                catch (HttpRequestException e)
+                {
+                    ModelState.AddModelError(string.Empty, "Request error. Please contact administrator.");
+                }
+            }
 
             if (emp == null)
             {
-				using (HttpClient client = new HttpClient())
-				{
-					try
-					{
-						HttpResponseMessage res = await client.GetAsync(_apiUrl + "Owner/Phone/" + employee.Phone);
-						if (res.IsSuccessStatusCode)
-						{
-							string data = await res.Content.ReadAsStringAsync();
-							owner = JsonConvert.DeserializeObject<Owner>(data);
-						}
-						else
-						{
-							ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
-						}
-					}
-					catch (HttpRequestException e)
-					{
-						ModelState.AddModelError(string.Empty, "Request error. Please contact administrator.");
-					}
-				}
-				if (owner == null)
-				{
-					return RedirectToAction("Login", "Home");
-				}
-				else
-				{
-					string otp = new Random().Next(000000, 999999).ToString();
+                using (HttpClient client = new HttpClient())
+                {
+                    try
+                    {
+                        HttpResponseMessage res = await client.GetAsync(_apiUrl + "Owner/Phone/" + employee.Phone);
+                        if (res.IsSuccessStatusCode)
+                        {
+                            string data = await res.Content.ReadAsStringAsync();
+                            owner = JsonConvert.DeserializeObject<Owner>(data);
+                        }
+                        else
+                        {
+                            ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                        }
+                    }
+                    catch (HttpRequestException e)
+                    {
+                        ModelState.AddModelError(string.Empty, "Request error. Please contact administrator.");
+                    }
+                }
+                if (owner == null)
+                {
+                    return RedirectToAction("Login", "Home");
+                }
+                else
+                {
+                    string otp = new Random().Next(000000, 999999).ToString();
 
-					otp = "123456"; // For testing only, remove this line in production
+                    otp = "123456"; // For testing only, remove this line in production
 
-					HttpContext.Session.SetString("OTP", otp);
+                    HttpContext.Session.SetString("OTP", otp);
 
-					/*// Send OTP via Twilio
+                    /*// Send OTP via Twilio
 					var accountSid = "ACd5083d30edb839433981a766a0c2e2fd";
 					var authToken = "";
 					TwilioClient.Init(accountSid, authToken);
@@ -120,27 +121,28 @@ namespace AzzanOrder.ManagerOwner.Controllers
 					};
 					MessageResource.Create(messageOptions);*/
 
-					// Redirect to the OTP input page
-					var model = new AzzanOrder.ManagerOwner.Models.Model { owner = owner, employee = emp };
+                    // Redirect to the OTP input page
+                    var model = new AzzanOrder.ManagerOwner.Models.Model { owner = owner, employee = emp };
 
-					var ownerJson = JsonConvert.SerializeObject(owner);
-					HttpContext.Response.Cookies.Append("LoginInfo", ownerJson, new CookieOptions
-					{
-						Expires = DateTimeOffset.UtcNow.AddDays(30)
-					});
+                    var ownerJson = JsonConvert.SerializeObject(owner);
+                    HttpContext.Response.Cookies.Append("LoginInfo", ownerJson, new CookieOptions
+                    {
+                        Expires = DateTimeOffset.UtcNow.AddDays(30)
+                    });
 
-					return View("OTPInput", model);
-					
-				}
+                    return View("OTPInput", model);
+
+                }
             }
-            else { 
+            else
+            {
                 string otp = new Random().Next(000000, 999999).ToString();
 
                 otp = "123456"; // For testing only, remove this line in production
 
-				HttpContext.Session.SetString("OTP", otp);
+                HttpContext.Session.SetString("OTP", otp);
 
-				/*// Send OTP via Twilio
+                /*// Send OTP via Twilio
                 var accountSid = "ACd5083d30edb839433981a766a0c2e2fd";
                 var authToken = "";
                 TwilioClient.Init(accountSid, authToken);
@@ -151,57 +153,56 @@ namespace AzzanOrder.ManagerOwner.Controllers
                 };
                 MessageResource.Create(messageOptions);*/
 
-				// Redirect to the OTP input page
-				var model = new AzzanOrder.ManagerOwner.Models.Model { employee = emp };
+                // Redirect to the OTP input page
+                var model = new AzzanOrder.ManagerOwner.Models.Model { employee = emp };
 
-				var empJson = JsonConvert.SerializeObject(emp);
-				HttpContext.Response.Cookies.Append("LoginInfo", empJson, new CookieOptions
-				{
-					Expires = DateTimeOffset.UtcNow.AddDays(30)
-				});
+                var empJson = JsonConvert.SerializeObject(emp);
+                HttpContext.Response.Cookies.Append("LoginInfo", empJson, new CookieOptions
+                {
+                    Expires = DateTimeOffset.UtcNow.AddDays(30)
+                });
 
-				return View("OTPInput", model);
-			}
-		}
+                return View("OTPInput", model);
+            }
+        }
 
-		[HttpPost]
-		public IActionResult VerifyOtp(string otp)
-		{
-			var sessionOtp = HttpContext.Session.GetString("OTP");
-			// Check if the OTP matches
-			if (otp == sessionOtp?.ToString())
-			{
-				TempData.Remove("OTP");
-				try
-				{
-					HttpContext.Request.Cookies.TryGetValue("LoginInfo", out string loginInfoJson);
-					var loginInfo = JsonConvert.DeserializeObject<Employee>(loginInfoJson);
-					if (loginInfo.RoleId != null)
-					{
-						return RedirectToAction("List", "Employee");
-					}
-					else
-					{
-						return RedirectToAction("Index", "Home");
-					}
-				}
-				catch
-				{
-					
-				}
+        [HttpPost]
+        public IActionResult VerifyOtp(string otp)
+        {
+            var sessionOtp = HttpContext.Session.GetString("OTP");
+            // Check if the OTP matches
+            if (otp == sessionOtp?.ToString())
+            {
+                TempData.Remove("OTP");
+                try
+                {
+                    HttpContext.Request.Cookies.TryGetValue("LoginInfo", out string loginInfoJson);
+                    var loginInfo = JsonConvert.DeserializeObject<Employee>(loginInfoJson);
+                    if (loginInfo.RoleId != null)
+                    {
+                        return RedirectToAction("List", "Employee");
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                catch
+                {
 
-				return View("OTPInput");
-			}
-			else
-			{
+                }
+                return View("OTPInput");
+            }
+            else
+            {
                 var model = new AzzanOrder.ManagerOwner.Models.Model();
-				ModelState.AddModelError(string.Empty, "Invalid OTP. Please try again.");
-				return View("OTPInput", model);
-			}
-		}
+                ModelState.AddModelError(string.Empty, "Invalid OTP. Please try again.");
+                return View("OTPInput", model);
+            }
+        }
 
 
-		[HttpPost]
+        [HttpPost]
         public async Task<IActionResult> LogoutAction()
         {
             HttpContext.Response.Cookies.Delete("LoginInfo");
@@ -210,29 +211,48 @@ namespace AzzanOrder.ManagerOwner.Controllers
 
 
 
-        public IActionResult Subscribe()
+        public async Task<IActionResult> SubscribeAsync()
         {
-            return View();
+            Api.Bank theBank = new Api.Bank();
+            using (System.Net.Http.HttpClient httpClient = new System.Net.Http.HttpClient())
+            {
+                var htmlData = await httpClient.GetStringAsync("https://api.vietqr.io/v2/banks");
+                theBank = JsonConvert.DeserializeObject<Api.Bank>(htmlData);
+            }
+
+            var model = new AzzanOrder.ManagerOwner.Models.Model { bankDatums = theBank.data };
+
+
+
+
+            return View(model);
         }
 
-		public IActionResult SubscribeMonthlyAction()
-		{
+        [HttpPost]
+        public IActionResult SubscribeAction(string pack, Model model)
+        {
+            // Implement necessary subscription logic here
+            // For example, process the subscription based on the pack type
 
-			return View();
-		}
-
-		public IActionResult SubscribeYearlyAction()
-		{
-			return View();
-		}
-
-		public IActionResult SubscribeForeverAction()
-		{
-			return View();
-		}
-
-
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+            switch (pack)
+            {
+                case "monthly":
+                    return Redirect("http://localhost:5173/?tableqr=");
+                    break;
+                case "yearly":
+                    // Handle yearly subscription
+                    break;
+                case "forever":
+                    // Handle forever subscription
+                    break;
+                default:
+                    // Handle invalid pack value if necessary
+                    break;
+            }
+            // Redirect to a confirmation page or wherever you'd like
+            return RedirectToAction("SubscriptionConfirmation");
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
--- payos/Controllers/CheckoutController.cs
+++ payos/Controllers/CheckoutController.cs
@@ -16,91 +16,114 @@ public class CheckoutController : Controller
 		_httpContextAccessor = httpContextAccessor;
 	}
 
-	//https://localhost:3002/?tableqr=QR_002/1&Price=1000&Item=OASItem&Message=Order
-	[HttpGet("/")]
-	public async Task<IActionResult> IndexAsync([FromQuery] string tableqr,[FromQuery] string Item,[FromQuery] double Price,[FromQuery] string Message)
-	{
-		Console.WriteLine("sfsdfsf " + tableqr);
-		Console.WriteLine("Item: " + Item);
-		Console.WriteLine("Price: " + Price);
-		Console.WriteLine("Message: " + Message);
 
-		// Save the 'tableqr' value to a cookie
-		if (!string.IsNullOrEmpty(tableqr))
-		{
-			Response.Cookies.Append("tableqrPayOs", tableqr, new CookieOptions
-			{
-				HttpOnly = true, // Makes the cookie accessible only through HTTP, not JavaScript
-				Expires = DateTimeOffset.UtcNow.AddDays(1) // Set the cookie expiration time
-			});
-		}
+    //https://localhost:3002/?tableqr=QR_002/1&Price=1000&Item=OASItem&Message=Order
+    //https://localhost:3002/?Price=30000&Item=SubscribeMontlyPack&Message=MonthyPack
+    [HttpGet("/")]
+    public async Task<IActionResult> IndexAsync([FromQuery] string tableqr, [FromQuery] string Item, [FromQuery] double Price, [FromQuery] string Message)
+    {
+        Console.WriteLine("tableqr: " + tableqr);
+        Console.WriteLine("Item: " + Item);
+        Console.WriteLine("Price: " + Price);
+        Console.WriteLine("Message: " + Message);
 
-		try
-		{
-			int orderCode = int.Parse(DateTimeOffset.Now.ToString("ffffff"));
-			ItemData item = new ItemData(Item, 1, 1000);
-			List<ItemData> items = new List<ItemData> { item };
+        // Save 'tableqr' and 'Item' to cookies
+        if (!string.IsNullOrEmpty(tableqr))
+        {
+            Response.Cookies.Append("tableqrPayOs", tableqr, new CookieOptions
+            {
+                HttpOnly = true,
+                Expires = DateTimeOffset.UtcNow.AddDays(1)
+            });
+        }
 
-			// Get the current request's base URL
-			var request = _httpContextAccessor.HttpContext.Request;
-			var baseUrl = $"{request.Scheme}://{request.Host}";
+        Response.Cookies.Append("ItemType", Item, new CookieOptions
+        {
+            HttpOnly = true,
+            Expires = DateTimeOffset.UtcNow.AddDays(1)
+        });
 
-			PaymentData paymentData = new PaymentData(
-				orderCode,
-				(int)Price,
-				Message + orderCode,
-				items,
-				$"{baseUrl}/cancel",
-				$"{baseUrl}/success"
-			);
+        try
+        {
+            int orderCode = int.Parse(DateTimeOffset.Now.ToString("ffffff"));
+            ItemData item = new ItemData(Item, 1, 1000);
+            List<ItemData> items = new List<ItemData> { item };
 
-			CreatePaymentResult createPayment = await _payOS.createPaymentLink(paymentData);
+            var request = _httpContextAccessor.HttpContext.Request;
+            var baseUrl = $"{request.Scheme}://{request.Host}";
 
-			return Redirect(createPayment.checkoutUrl);
-		}
-		catch (System.Exception exception)
-		{
-			Console.WriteLine(exception);
-			return Redirect("/");
-		}
-	}
+            PaymentData paymentData = new PaymentData(
+                orderCode,
+                (int)Price,
+                Message + orderCode,
+                items,
+                $"{baseUrl}/cancel",
+                $"{baseUrl}/success"
+            );
 
-	[HttpGet("/cancel")]
-	public IActionResult Cancel()
-	{
-		//return Redirect("http://localhost:5173/");
-		HttpContext.Request.Cookies.TryGetValue("tableqrPayOs", out string tableqr);
-		return Redirect("http://localhost:5173/?tableqr=" + tableqr + "&status=success");
-	}
+            CreatePaymentResult createPayment = await _payOS.createPaymentLink(paymentData);
 
-	[HttpGet("/success")]
-	public async Task<ActionResult> Success([FromQuery] int memberId)
-	{
-		HttpContext.Request.Cookies.TryGetValue("tableqrPayOs", out string tableqr);
-		//var _apiUrl = $"https://localhost:7183/api/Member/";
+            return Redirect(createPayment.checkoutUrl);
+        }
+        catch (System.Exception exception)
+        {
+            Console.WriteLine(exception);
+            return Redirect("/");
+        }
+    }
 
-  //      using (HttpClient client = new HttpClient())
-  //      {
-  //          try
-  //          {
-  //              HttpResponseMessage res = await client.GetAsync(_apiUrl + memberId);
-  //              if (res.IsSuccessStatusCode)
-  //              {
-  //                  string data = await res.Content.ReadAsStringAsync();
-  //                  roles = JsonConvert.DeserializeObject<List<Role>>(data);
-  //              }
-  //              else
-  //              {
-  //                  // Handle the error response here
-  //                  ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
-  //              }
-  //          }
-  //          catch (HttpRequestException e)	
-  //          {
-  //              // Handle the exception here
-  //              ModelState.AddModelError(string.Empty, "Request error. Please contact administrator.");
-  //          }
-  //      }
-        return Redirect("http://localhost:5173/?tableqr=" + tableqr + "&status=success");
-	}
+
+
+    [HttpGet("/cancel")]
+    public IActionResult Cancel()
+    {
+        HttpContext.Request.Cookies.TryGetValue("tableqrPayOs", out string tableqr);
+        HttpContext.Request.Cookies.TryGetValue("ItemType", out string itemType);
+
+        if (itemType.Contains("Subscribe"))
+        {
+            return RedirectToAction("Subscribe", "Home");
+        }
+
+        return Redirect("http://localhost:5173/?tableqr=" + tableqr + "&status=cancel");
+    }
+
+
+    [HttpGet("/success")]
+    public async Task<IActionResult> Success([FromQuery] int memberId)
+    {
+        HttpContext.Request.Cookies.TryGetValue("tableqrPayOs", out string tableqr);
+        HttpContext.Request.Cookies.TryGetValue("ItemType", out string itemType);
+
+        if (itemType.Contains("Subscribe"))
+        {
+            return RedirectToAction("Subscribe", "Home");
+        }
+        return RedirectToAction("Index", "Home");
+    }
+
+
+    //var _apiUrl = $"https://localhost:7183/api/Member/";
+    //      using (HttpClient client = new HttpClient())
+    //      {
+    //          try
+    //          {
+    //              HttpResponseMessage res = await client.GetAsync(_apiUrl + memberId);
+    //              if (res.IsSuccessStatusCode)
+    //              {
+    //                  string data = await res.Content.ReadAsStringAsync();
+    //                  roles = JsonConvert.DeserializeObject<List<Role>>(data);
+    //              }
+    //              else
+    //              {
+    //                  // Handle the error response here
+    //                  ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+    //              }
+    //          }
+    //          catch (HttpRequestException e)	
+    //          {
+    //              // Handle the exception here
+    //              ModelState.AddModelError(string.Empty, "Request error. Please contact administrator.");
+    //          }
+    //      }
 }
